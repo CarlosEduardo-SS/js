4bee5a17ed0eb110bf04b8ffbfd6d69ca37f501f
carlos <carloseduardosilvasantos2008@gmail.com>
2026-07-16 21:58:58 -0300

    incluindo novos arquivos

--- aula_5/aula_5.js
+++ aula_5/aula_5.js
@@ -1,10 +1,15 @@
 const GLOBAL_URL = "https://6a5032c2f45d5352b6121b52.mockapi.io/produtos";
 
 async function carregarProdutos() {
-  const resposta = await fetch(GLOBAL_URL);
-  const produtos = await resposta.json();
+  try {
+    const resposta = await fetch(GLOBAL_URL);
+    const produtos = await resposta.json();
 
-  listarProdutos(produtos);
+    listarProdutos(produtos);
+  } catch (error) {
+    console.error(error);
+    alert("Não foi possível carregar os dados.");
+  }
 }
 
 function listarProdutos(produtos) {
@@ -15,9 +20,13 @@ function listarProdutos(produtos) {
         <td>${produto.id}</td>
         <td>${produto.nome}</td>
         <td>${produto.quantidade}</td>
-        <td>${produto.valorUnitario}</td>
-        <td>${calcularTotal(produto.quantidade, produto.valorUnitario)}</td>
-        <td></td>
+        <td>${produto.preco}</td>
+        <td>${calcularTotal(produto.quantidade, produto.preco)}</td>
+        <td>
+          <button class="btn btn-danger" onclick="removerProduto(${produto.id})">
+          Remover
+          </button>
+        </td>
       </tr>
     `;
   }
@@ -26,8 +35,50 @@ function listarProdutos(produtos) {
   tbody.innerHTML = html;
 }
 
-function calcularTotal(quantidade, valorUnitario) {
-  return "VALOR CALCULADO";
+function calcularTotal(quantidade, preco) {
+  return quantidade * preco;
+}
+
+async function removerProduto(id) {
+  if (!confirm("Realmente deseja apagar esse produto?")) {
+    return;
+  }
+
+  const url = `${GLOBAL_URL}/${id}`;
+
+  try {
+    await fetch(url, {
+      method: "DELETE",
+    });
+  } catch (error) {
+    console.error(error);
+    alert("Não foi possível carregar os dados.");
+  } finally {
+    carregarProdutos();
+  }
+}
+
+async function cadastrarProduto() {
+  const produto = {
+    nome: document.querySelector("#nome").value,
+    preco: Number(document.querySelector("#preco").value),
+    quantidade: Number(document.querySelector("#quantidade").value),
+  };
+
+  try {
+    await fetch(GLOBAL_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "Application/json",
+      },
+      body: JSON.stringify(produto),
+    });
+
+    carregarProdutos();
+  } catch (error) {
+    console.error(error);
+    alert("Não foi possível cadastrar esse produto");
+  }
 }
 
 carregarProdutos();
